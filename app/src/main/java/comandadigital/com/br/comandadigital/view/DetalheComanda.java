f188b6ba5341afd356b77038a008a6cc74fec92b
guilh <guilhermeaf11@hotmail.com>
2017-05-14 16:17:08 -0300

    #1

---
+++ app/src/main/java/comandadigital/com/br/comandadigital/view/DetalheComanda.java
@@ -0,0 +1,15 @@
+package comandadigital.com.br.comandadigital.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comandadigital.com.br.comandadigital.R;
+
+public class DetalheComanda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comanda);
+    }
+}
